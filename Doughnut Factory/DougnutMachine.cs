9a49ef68cf4b1064d0311d0f8176f0c64118cd66
screw4445 <jazeps.bumbiers@gmail.com>
2018-04-15 16:34:55 +0300

    ...

---
+++ Doughnut Factory/DougnutMachine.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Doughnut_Factory
+{
+    public partial class DougnutMachine : Component
+    {
+        public DougnutMachine()
+        {
+            InitializeComponent();
+        }
+
+        public DougnutMachine(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+
+        }
+
+        public enum DougnutType
+        {
+            Glazed,
+            Sugar,
+            Chocolate,
+            ChocoladeCake,
+            Custard,
+            Grape,
+            Lemon,
+            PlainCake,
+            SugarCage
+        }
+
+        private DougnutType mFlavor;
+        public DougnutType Flavor
+        {
+            get
+            {
+                return mFlavor;
+            }
+
+            set
+            {
+                mFlavor = value;
+            }
+
+        }
+
+        public class Dougnut
+        {
+            private DougnutType mFlavor;
+            private float mPrice = .50F;
+            private readonly System.DateTime mTimeofCreation;
+
+            public DougnutType Flavor
+            {
+                get
+                {
+                    return mFlavor;
+                }
+
+                set
+                {
+                    mFlavor = value;
+                }
+
+            }
+
+            public float Price
+            {
+                get
+                {
+                    return mPrice;
+                }
+
+                set
+                {
+                    mPrice = value;
+                }
+
+            }
+
+            public System.DateTime timeofCreation
+            {
+                get
+                {
+                    return mTimeofCreation;
+                }
+            }
+
+            public Dougnut(DougnutType Flavor)
+            {
+                mTimeofCreation = System.DateTime.Now;
+                mFlavor = Flavor;
+            }
+
+        }
+
+        private System.Collections.ArrayList mDoughnuts = new System.Collections.ArrayList();
+        public Dougnut this[int Index]
+        {
+            get
+            {
+                return (Dougnut)mDoughnuts[Index];
+            }
+            set
+            {
+                mDoughnuts[Index] = value;
+            }
+        }
+
+        //izveido pashu delegatu
+        public delegate void DoughnutCompliteDelegate();
+        public event DoughnutCompliteDelegate DoughnuntComplite;
+
+        private void timerName_Tick(object sender, EventArgs e)
+        {
+            Dougnut mDoughnut = new Dougnut(this.Flavor);
+            mDoughnuts.Add(mDoughnut);
+            DoughnuntComplite();
+        }
+
+        public bool Enable
+        {
+            set
+            {
+                timerName.Enabled = value;
+            }
+        }
+
+        public int Inderval
+        {
+            set
+            {
+                timerName.Interval = value;
+            }
+        }
+
+        public void MakeDoughnuts (DougnutType dFlavor)
+        {
+            Flavor = dFlavor;
+            switch(dFlavor)
+            {
+                case DougnutType.Chocolate:
+                    Inderval = 2000;
+                    break;
+                case DougnutType.ChocoladeCake:
+                    Inderval = 10000;
+                    break;
+                case DougnutType.Custard:
+                    Inderval = 8000;
+                    break;
+                case DougnutType.Glazed:
+                    Inderval = 10000;
+                    break;
+                case DougnutType.Grape:
+                    Inderval = 10000;
+                    break;
+                case DougnutType.Lemon:
+                    Inderval = 8000;
+                    break;
+                case DougnutType.PlainCake:
+                    Inderval = 10000;
+                    break;
+                case DougnutType.Sugar:
+                    Inderval = 1000;
+                    break;
+                case DougnutType.SugarCage:
+                    Inderval = 10000;
+                    break;
+            }
+            Enable = true;
+        }
+
+    }
+}
---
+++ Doughnut Factory/Main.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Doughnut_Factory
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private DougnutMachine myDoughnutMachine;
+
+        private int mRaisedGlazed;
+        private int mRaisedSugar;
+        private int mRaisedChocolate;
+
+        private int mCakePlain;
+        private int mCakeChocolate;
+        private int mCakeSugar;
+
+        private int mFilledLemon;
+        private int mFilledGrapge;
+        private int mFilledCustard;
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtQuantity_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar) == false)
+                errorProvider1.SetError(txtQuantity, "Lūdzu ievadi tikai ciparus");
+            else
+                errorProvider1.SetError(txtQuantity, "");
+        }
+
+        private void txtQuantity_Validating(object sender, CancelEventArgs e)
+        {
+            if (txtQuantity.Text == "")
+            {
+                errorProvider1.SetError(txtQuantity, "Lūdzu ievadi daudzumu!");
+                e.Cancel = true;
+            }
+            else if (errorProvider1.GetError(txtQuantity) != "")
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                errorProvider1.SetError(txtQuantity, "");
+            }
+        }
+
+        private void txtQuantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            myDoughnutMachine = new DougnutMachine();
+            myDoughnutMachine.DoughnuntComplite += new DougnutMachine.DoughnutCompliteDelegate(DoughnutCompliteHandeler);
+      
+        }
+
+        private void mnuRaisedGlazed_Clicked(object sender, EventArgs e)
+        {
+            menuRaisedGlazzed.Checked = true;
+            menuRaisedChocolade.Checked = false;
+            menuRaisedSugar.Checked = false;
+
+            myDoughnutMachine.MakeDoughnuts(DougnutMachine.DougnutType.Glazed);
+        }
+
+        private void menuRaisedSugar_Click(object sender, EventArgs e)
+        {
+            menuRaisedGlazzed.Checked = true;
+            menuRaisedChocolade.Checked = true;
+            menuRaisedSugar.Checked = false;
+
+            myDoughnutMachine.MakeDoughnuts(DougnutMachine.DougnutType.Sugar);
+
+        }
+
+        private void menuRaisedChocolade_Click(object sender, EventArgs e)
+        {
+            menuRaisedGlazzed.Checked = false;
+            menuRaisedChocolade.Checked = false;
+            menuRaisedSugar.Checked = true;
+
+            myDoughnutMachine.MakeDoughnuts(DougnutMachine.DougnutType.Chocolate);
+        }
+
+        private void DoughnutCompliteHandeler()
+        {
+            switch (myDoughnutMachine.Flavor)
+            {
+                case DougnutMachine.DougnutType.Glazed:
+                    mRaisedGlazed++;
+                    txtGlazzedRaised.Text = mRaisedGlazed.ToString();
+                    break;
+                case DougnutMachine.DougnutType.Sugar:
+                    mRaisedSugar++;
+                    txtSugarRaised.Text = mRaisedSugar.ToString();
+                    break;
+                case DougnutMachine.DougnutType.Chocolate:
+                    mRaisedChocolate++;
+                    txtChocolateRaised.Text = mRaisedChocolate.ToString();
+                    break;
+            }
+        }
+
+        private void menuStop_Click(object sender, EventArgs e)
+        {
+            myDoughnutMachine.Enable = true;
+        }
+    }
+}
